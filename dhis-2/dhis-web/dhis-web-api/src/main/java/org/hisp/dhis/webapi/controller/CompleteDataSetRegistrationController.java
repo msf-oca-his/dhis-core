f6c24e3dcc85ca91114cd3d846a0414409dcf778
Srikanth Katta <srikantkatta2207@gmail.com>
2018-02-22 18:52:21 +0530

    [Srikanht/kaustav] added completness full func

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/CompleteDataSetRegistrationController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/CompleteDataSetRegistrationController.java
@@ -309,6 +309,25 @@ public class CompleteDataSetRegistrationController
     // Supportive methods
     // -------------------------------------------------------------------------
 
+    private void addRegistrationsForOrgUnits( List<CompleteDataSetRegistration> registrations, Set<OrganisationUnit> organisationUnits, DataSet dataSet, Period period,
+        DataElementCategoryOptionCombo attributeOptionCombo, String storedBy, Date completionDate, Date lastUpdated, String lastUpdatedBy, boolean isCompleted )
+        throws WebMessageException
+    {
+        for ( OrganisationUnit ou : organisationUnits )
+        {
+            if ( ou.getDataSets().contains( dataSet ) )
+            {
+                CompleteDataSetRegistration registration =
+                    registerCompleteDataSet( dataSet, period, ou, attributeOptionCombo, storedBy, completionDate, lastUpdated, lastUpdatedBy, isCompleted );
+
+                if ( registration != null )
+                {
+                    registrations.add( registration );
+                }
+            }
+        }
+    }
+
     private void asyncImport( ImportOptions importOptions, String format, HttpServletRequest request, HttpServletResponse response )
         throws IOException
     {
